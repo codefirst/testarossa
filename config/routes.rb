986f4ce51b7670338ea2860854cda6169152cc4a
Ryo SUETSUGU <suetsugu.r@gmail.com>
2014-07-20 15:52:27 +0900

    Added: specs

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :test_cases
+
   resources :projects
 
   get 'top/index'
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140720023242) do
+ActiveRecord::Schema.define(version: 20140720064433) do
 
   create_table "projects", force: true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "test_cases", force: true do |t|
+    t.string   "title"
+    t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
