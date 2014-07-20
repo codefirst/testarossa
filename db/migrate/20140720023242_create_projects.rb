8029b16dbd2d69cb4898311623742bca94737fe5
Ryo SUETSUGU <suetsugu.r@gmail.com>
2014-07-20 14:17:48 +0900

    Added: authenticate before access

--- db/migrate/20140720023242_create_projects.rb
+++ db/migrate/20140720023242_create_projects.rb
@@ -1,10 +1,8 @@
 class CreateProjects < ActiveRecord::Migration
   def change
     create_table :projects do |t|
-      t.integer :user_id
       t.string :name
       t.timestamps
     end
-    add_index :projects, :user_id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,13 +14,10 @@
 ActiveRecord::Schema.define(version: 20140720023242) do
 
   create_table "projects", force: true do |t|
-    t.integer  "user_id"
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  add_index "projects", ["user_id"], name: "index_projects_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "nickname",   null: false
