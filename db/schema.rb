32012381e76ce8cefa30ec86b36582cf49bf8956
Ryo SUETSUGU <suetsugu.r@gmail.com>
2014-07-23 10:48:04 +0900

    Added: description field to projects

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20140720100152) do
 
   create_table "projects", force: true do |t|
     t.string   "name"
+    t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
